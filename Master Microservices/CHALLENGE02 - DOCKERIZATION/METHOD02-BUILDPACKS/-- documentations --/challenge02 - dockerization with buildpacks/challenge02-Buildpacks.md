e957392d814da6d296c52ab9277184e37a17d845
davidvera13 <david.vera@9online.fr>
2024-10-29 18:41:36 +0100

    feat(ms): challenge02 - dockerization using buildpacks implementation

---
+++ Master Microservices/CHALLENGE02 - DOCKERIZATION/METHOD02-BUILDPACKS/-- documentations --/challenge02 - dockerization with buildpacks/challenge02-Buildpacks.md
@@ -0,0 +1,14 @@
+## Notes
+
+### Using buildpacks
+we can generate docker image using  single mvn command line
+
+    mvn spring-boot:build-image
+
+Note : we may have issue seeing containers running in docker desktop. 
+We can list containers with 
+
+    docker ps -a
+
+
+![images/img_14.png](../images/img_14.png)
---
+++ Master Microservices/CHALLENGE02 - DOCKERIZATION/METHOD02-BUILDPACKS/runme.bat
@@ -0,0 +1,31 @@
+@echo off
+
+cd "./accounts"
+call mvn clean
+call mvn spring-boot:build-image
+cd ".."
+
+cd "./cards"
+call mvn clean
+call mvn spring-boot:build-image
+cd ".."
+
+cd "./loans"
+call mvn clean
+call mvn spring-boot:build-image
+cd ".."
+
+@REM we do not push for the moment ..
+@REM call docker image push docker.io/davidvera/accounts:0.0.1-SNAPSHOT
+@REM call docker image push docker.io/davidvera/cards:0.0.1-SNAPSHOT
+@REM call docker image push docker.io/davidvera/loans:0.0.1-SNAPSHOT
+
+@REM if we remove local image, we can get them with pull
+@REM call docker pull davidvera/accounts:0.0.1-SNAPSHOT
+@REM call docker pull davidvera/cards:0.0.1-SNAPSHOT
+@REM call docker pull davidvera/loans:0.0.1-SNAPSHOT
+
+@REM Running directly, will pull image if not exists
+call docker run -d -p 8080:8080 davidvera/accounts:0.0.1-SNAPSHOT
+call docker run -d -p 9000:9000 davidvera/cards:0.0.1-SNAPSHOT
+call docker run -d -p 8090:8090 davidvera/loans:0.0.1-SNAPSHOT
