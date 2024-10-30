eb0fac0942b4a2be974fd5b3524ba46da7750285
davidvera13 <david.vera@9online.fr>
2024-10-30 23:23:57 +0100

    feat(ms): challenge02 - dockerization - adding docker compose

--- Master Microservices/CHALLENGE02 - DOCKERIZATION/METHOD01-DOCKERFILE/runme.bat
+++ Master Microservices/CHALLENGE02 - DOCKERIZATION/METHOD01-DOCKERFILE/runme.bat
@@ -2,30 +2,34 @@
 
 cd "./accounts"
 call mvn clean
-call docker build . -t davidvera/accounts:0.0.1-SNAPSHOT
+call docker build . -t davidvera/accounts:0.0.2-SNAPSHOT
 cd ".."
 
 cd "./cards"
 call mvn clean
-call docker build . -t davidvera/cards:0.0.1-SNAPSHOT
+call docker build . -t davidvera/cards:0.0.2-SNAPSHOT
 cd ".."
 
 cd "./loans"
 call mvn clean
-call docker build . -t davidvera/loans:0.0.1-SNAPSHOT
+call docker build . -t davidvera/loans:0.0.2-SNAPSHOT
 cd ".."
 
 @REM we do not push for the moment ..
-@REM call docker image push docker.io/davidvera/accounts:0.0.1-SNAPSHOT
-@REM call docker image push docker.io/davidvera/cards:0.0.1-SNAPSHOT
-@REM call docker image push docker.io/davidvera/loans:0.0.1-SNAPSHOT
+@REM call docker image push docker.io/davidvera/accounts:0.0.2-SNAPSHOT
+@REM call docker image push docker.io/davidvera/cards:0.0.2-SNAPSHOT
+@REM call docker image push docker.io/davidvera/loans:0.0.2-SNAPSHOT
 
 @REM if we remove local image, we can get them with pull
-@REM call docker pull davidvera/accounts:0.0.1-SNAPSHOT
-@REM call docker pull davidvera/cards:0.0.1-SNAPSHOT
-@REM call docker pull davidvera/loans:0.0.1-SNAPSHOT
+@REM call docker pull davidvera/accounts:0.0.2-SNAPSHOT
+@REM call docker pull davidvera/cards:0.0.2-SNAPSHOT
+@REM call docker pull davidvera/loans:0.0.2-SNAPSHOT
 
 @REM Running directly, will pull image if not exists
-call docker run -d -p 8080:8080 davidvera/accounts:0.0.1-SNAPSHOT
-call docker run -d -p 9000:9000 davidvera/cards:0.0.1-SNAPSHOT
-call docker run -d -p 8090:8090 davidvera/loans:0.0.1-SNAPSHOT
+@REM call docker run -d -p 8080:8080 davidvera/accounts:0.0.2-SNAPSHOT
+@REM call docker run -d -p 9000:9000 davidvera/cards:0.0.2-SNAPSHOT
+@REM call docker run -d -p 8090:8090 davidvera/loans:0.0.2-SNAPSHOT
+
+cd "./-- docker --"
+call docker compose up -d
+cd ".."
